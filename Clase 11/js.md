17afe092fad0e524c0a45ca8b4f4386d03e88a00
leo.matt <leo_eles@hotmail.com>
2024-05-06 22:02:41 -0300

    clase 11

---
+++ Clase 11/js.md
@@ -0,0 +1,4 @@
+### Que es javascript?
+
+Es un lenguaje de programacion interpretado por el v8engine. Es un lenguaje  de alto nivel (muy parecido al lenguaje del ser humano).
+SE EJECUTA EN EL NAVEGADOR
---
+++ Clase 11/script.js
@@ -0,0 +1,65 @@
+/* TIPOS DE DATOS */
+
+/* Datos ptimitivos
+Number = numeros
+string = debe ir entre comillas ; textos, parrafos, nombres 
+boolean = valores verdad o falso
+null = Ausencia o nulidad de un dato
+undefined = No hay un dato definido 
+symbol
+
+
+Ejemplo: Soy un mensajero que envia una carta, llego a la casa, me atienden y me dicen que NO quieren recibir la carta
+estado_de_respuesta: false
+
+Ejemplo: Soy un mensajero que envia una carta, NO llego a la casa.
+estado_de_respuesta: undefined
+
+Ejemplo: Soy un mensajero que envia una carta, llego a la casa, NO me atienden.
+estado_de_respuesta: null
+
+
+*/
+/* Operadores aritmeticos
++: Concatenacion
+devuelve string
+
+Siempre devuelven numeros
++ 
+-
+ *
+/
+% resto division o modulo
+Undefined + 1 = NaN (soy un numero pero indefinido)
+
+*/
+/* Constructores
+String(dato) transforma mi dato a un string
+Number(dato) transforma mi dato a un number
+Boolean(dato) transforma mi dato a un booleando 
+*/
+
+/* falsy o truthy
+
+valores falsos: 0; ""; false; null; undefined; NaN*/
+/* Datos objetos: Proximamente */
+
+
+
+/* comparadores
+
+siempre devuelven booleanos
+
+== igualdad
+=== estricta igualdad (Compara datos)
+!= diferencia
+!== estricta diferencia
+> mayor
+< menor
+>=
+<=
+*/
+/* funciones nativas
+
+typeof() tipo de dato
+isNaN() devuelve booleano si es NaN o no*/
